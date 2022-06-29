db26234fc5c1d38ab3b5ec95fcd08b685085ee1c
PC-Ngumoha <pcngumoha@gmail.com>
2022-06-29 11:37:47 +0000

    Update 3-print_alphabet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for i in range(0, 26):
     if i == 4 or i == 16:
         continue
     else:
-        print(f"{chr(ord('a') + i):s}", end="")
+        print("{:s}".format(chr(ord('a') + i),), end="")
