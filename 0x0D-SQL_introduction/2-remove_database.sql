d24e6459ae8ed85370fd3a33cd4ba629bed1726b
PC-Ngumoha <pcngumoha@gmail.com>
2022-09-07 09:38:48 +0000

    Task 3: Create '3-list_tables.sql' file

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- 2-remove_database.sql
 -- removes a specific database from the server if it exists.
 -- If it does not exists, it does not raise an error
-DROP DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
