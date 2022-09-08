423b2be41e46cdc96b53f77fa757bee783cbc935
PC-Ngumoha <pcngumoha@gmail.com>
2022-09-08 11:04:05 +0000

    Task 1: Update '1-create_user.sql' file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Creates a user if it doesn't already exist and grants it all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
