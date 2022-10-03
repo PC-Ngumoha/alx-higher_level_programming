9ca2a393dec11b33a46f31921884bc3b6b742892
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-03 22:14:12 +0000

    Task 11: Update '11-second_biggest.js' file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const numbers = [];
 let i;
 
 for (i = 2; i < process.argv.length; i++) {
-  numbers.push(process.argv[i]);
+  numbers.push(parseInt(process.argv[i], 10));
 }
 
 const listLength = numbers.length;
@@ -13,13 +13,13 @@ const listLength = numbers.length;
 if (listLength <= 1) {
   console.log(0);
 } else {
-  let secondBest, best;
-  best = Math.max(...numbers);
+  let secondBest;
+  const best = Math.max(...numbers);
   secondBest = 0;
   for (i = 0; i < listLength; i++) {
-	if (numbers[i] > secondBest && numbers[i] != best) {
+    if ((numbers[i] > secondBest) && (numbers[i] !== best)) {
       secondBest = numbers[i];
-	}
+    }
   }
   console.log(secondBest);
 }
