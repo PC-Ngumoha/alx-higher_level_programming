1aeeed48fd5dc190952005ef87c21b2771a90a2d
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-05 07:16:25 +0000

    Advanced: Update '102-concat.js' file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,12 +6,10 @@ if (process.argv.length === 5) {
   const fileA = process.argv[2];
   const fileB = process.argv[3];
   const fileC = process.argv[4];
-  let content1;
-  let content2;
   // reading the contents of fileA
-  content1 = fs.readFileSync(fileA, 'utf-8');
+  const content1 = fs.readFileSync(fileA, 'utf-8');
   // reading the contents of fileB
-  content2 = fs.readFileSync(fileB, 'utf-8');
+  const content2 = fs.readFileSync(fileB, 'utf-8');
 
   // concatenating the content from both files
   const result = content1.concat(content2);
