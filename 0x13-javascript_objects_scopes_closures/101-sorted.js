2d7b406e94ded9aa5692e6d61da1b15e422d0af2
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-05 04:40:25 +0000

    Advanced: Update '101-sorted.js' file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-const newDict = {}
+const newDict = {};
 let maxOccur = 0;
 let sortable = [];
 
-//create an array from the dict
-for (user in dict) {
+// create an array from the dict
+for (const user in dict) {
   sortable.push([[user], dict[user]]);
 }
 
-//determine the max occurrence
+// determine the max occurrence
 for (let i = 0; i < sortable.length; i++) {
   if (maxOccur < sortable[i][1]) {
     maxOccur = sortable[i][1];
   }
 }
 
-//sort the array
+// sort the array
 sortable.sort((a, b) => {
-  return a[1] - b[1]; 
+  return a[1] - b[1];
 });
 
-//concatenate arrays with same distribution numbers
+// concatenate arrays with same distribution numbers
 const compressed = [];
 for (let occur = 1; occur <= maxOccur; occur++) {
   compressed[occur - 1] = [occur, []];
   for (let i = 0; i < sortable.length; i++) {
     if (occur === sortable[i][1]) {
       compressed[occur - 1][1] = compressed[occur - 1][1].concat(sortable[i][0]);
-	}
+    }
   }
 }
 sortable = compressed;
@@ -38,5 +38,5 @@ sortable.forEach((element) => {
   newDict[element[0]] = element[1];
 });
 
-//Prints out the new dictionary
+// Prints out the new dictionary
 console.log(newDict);
