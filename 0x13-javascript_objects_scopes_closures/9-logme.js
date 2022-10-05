9fdb764c9d2670c0f929dc58f9993a8289aae57a
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-05 03:20:57 +0000

    Task 9: Update '9-logme.js' file

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.logMe = function log (item) {
-  if (typeof log.count == 'undefined') log.count = 0;
+  if (typeof log.count === 'undefined') log.count = 0;
   console.log(`${log.count}: ${item}`);
   log.count++;
 };
