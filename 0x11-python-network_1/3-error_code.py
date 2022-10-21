5d38f01a59f930a763d1136e3fd226ca66b3dce8
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-21 15:47:11 +0000

    Task Complete: Create '3-error_code.py' script

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-'''A script that gets the value of a specific HTTP request header.'''
+'''A script that gets the body of a HTTP response'''
 from urllib.request import Request, urlopen
 import sys
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    if url != '':
-        req = Request(url)
-        try:
-            with urlopen(req) as response:
-                output = response.read()
-                print(output.decode('utf-8'))
-        except urllib.error.HTTPError as e:
-            print('Error Code: {}'.format(e.code))
+    req = Request(url)
+    try:
+        with urlopen(req) as response:
+            output = response.read()
+            print(output.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
