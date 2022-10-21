3cffac42c5a47ab2a4f309b24774f1abb3a98fbd
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-21 15:39:39 +0000

    Task Complete: Create '3-error_code.py' script

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,11 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    req = Request(url)
-    try:
-        with urlopen(req) as response:
-            output = response.read()
-            print(output.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print('Error Code: {}'.format(e.code))
+    if url != '':
+        req = Request(url)
+        try:
+            with urlopen(req) as response:
+                output = response.read()
+                print(output.decode('utf-8'))
+        except urllib.error.HTTPError as e:
+            print('Error Code: {}'.format(e.code))
