eb4ffeed24f5f9349e063c15875f82f463a104b8
PC-Ngumoha <pcngumoha@gmail.com>
2022-12-05 10:50:59 +0000

    Task Complete

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,4 @@
+// Modify the color of the header through the use of regular
+// javascript DOM manipulation.
 const header = document.querySelector('header');
-header.style.color = '#FF0000'
+header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Changes the color of the header sections contents
+// using the jQuery API.
+$(function () {
+  $('header').css({ color: '#FF0000' });
+});
