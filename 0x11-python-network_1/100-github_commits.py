31dd7d5e2aee3477e903d9a9631d01fa3f6b7fea
PC-Ngumoha <pcngumoha@gmail.com>
2023-01-17 18:45:27 +0000

    Task Complete: Create '100-github_commits.py' script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,17 +8,15 @@ import sys
 if __name__ == '__main__':
     repo_name = sys.argv[1]
     owner = sys.argv[2]
-    token = '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'
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner,
                                                               repo_name)
     params = {'per_page': 10}
-    headers = {'Authorization': 'Bearer {}'.format(token)}
     try:
-        r = requests.get(url, params=params, headers=headers)
-        responses = r.json()
-        for response in responses:
-            sha = response.get('sha')
-            author_name = response.get('commit').get('author').get('name')
+        r = requests.get(url, params=params)
+        commits = r.json()
+        for commit in commits:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
             print('{}: {}'.format(sha, author_name))
-    except (ValueError, TypeError) as err:
-        print(err)
+    except Exception:
+        pass
