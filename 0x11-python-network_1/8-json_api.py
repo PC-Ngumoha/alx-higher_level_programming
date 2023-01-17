e0794cf7ae2ba19ccba09b6b689ab4089fa4afb4
PC-Ngumoha <pcngumoha@gmail.com>
2023-01-17 15:15:22 +0000

    Task Complete: Create '8-json_api.py' python script

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Sending data to a server which generates a JSON response
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    arg = ""
+    if len(sys.argv) == 2:
+        arg = sys.argv[1]
+    output = requests.post(url, data={'q': arg})
+    try:
+        output = output.json()
+        if output == {}:
+            print('No result')
+        else:
+            print('[{}] {}'.format(output.get('id'), output.get('name')))
+    except ValueError:
+        print('Not a Valid JSON')
