8aea9f025fe31460ca7d4561f1f8dcd28245dd56
PC-Ngumoha <pcngumoha@gmail.com>
2023-01-17 01:52:31 +0000

    Update '101-post_json.sh' shell script

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends the contents of a JSON file in a POST request to a server.
-curl -X POST "$1" -H "Content-Type: application/json" -d @"$2"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
