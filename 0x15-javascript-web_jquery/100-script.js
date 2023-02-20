e3cb87f6231633e1a54fc4bcf2b325d566661245
PC-Ngumoha <pcngumoha@gmail.com>
2023-02-20 15:58:31 +0000

    Task Complete: Create '100-script.js' script

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/**
+ * 100-script.js: Changes the color of the header's contents to #FF0000
+ * using raw javascript
+ */
+window.addEventListener('load', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
