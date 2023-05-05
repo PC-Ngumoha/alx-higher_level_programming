0a0bf1940efb67376afed1acd8827b08e6e0007c
PC-Ngumoha <pcngumoha@gmail.com>
2023-05-05 15:11:00 +0000

    Complete

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- 7-cities.sql
+-- Creates a DB 'hbtn_0d_usa' and TABLE 'cities'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT NOT NULL UNIQUE AUTO_INCREMENT,
+name VARCHAR(256),
+state_id INT NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`state_id`) REFERENCES states(`id`)
+);
